31e5c5103169ed63fddae80bea806b738699ad20
Shun Ueda <me@shu.nu>
2026-06-18 17:17:53 -0400

    feat: aws profile

--- hosts/anterior/users/me.nix
+++ hosts/anterior/users/me.nix
@@ -1,1 +1,24 @@
-{ self, ... }: { imports = [ self.homeModules.common ]; }
+{ self, ... }: {
+  imports = [ self.homeModules.common ];
+  programs = {
+    awscli = {
+      enable = true;
+      settings =
+        let
+          session = "sso";
+        in
+        {
+          "sso-session ${session}" = {
+            sso_start_url = "https://identitycenter.amazonaws.com/ssoins-72233c6842aa3089";
+            sso_region = "us-east-1";
+            sso_registration_scopes = "sso:account:access";
+          };
+          "profile default" = {
+            sso_session = session;
+            sso_account_id = "038462749198";
+            sso_role_name = "UseSopsKey";
+          };
+        };
+    };
+  };
+}
