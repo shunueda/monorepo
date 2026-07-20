6e646beb4de0da0b44bceb4ae7b5b45a0b93181e
Shun Ueda <git@shunueda.org>
2026-07-20 10:20:14 -0400

    refactor: backport homerow from unstable

--- nix/ueda-overlay.nix
+++ nix/ueda-overlay.nix
@@ -3,16 +3,16 @@
 final: prev:
 let
   inherit (final.stdenv.hostPlatform) system;
+
+  pkgs-unstable = import inputs.nixpkgs-unstable {
+    inherit system;
+    config.allowUnfree = true;
+  };
 in
 {
   # Custom packages
-  inherit (self.packages.${system})
-    ns
-    homerow
-    displaymode
-    ueda-emacs
-    ;
+  inherit (self.packages.${system}) displaymode;
 
   # Backport from unstable
-  inherit (inputs.nixpkgs-unstable.legacyPackages.${system}) emacs31;
+  inherit (pkgs-unstable) emacs31 homerow;
 }
