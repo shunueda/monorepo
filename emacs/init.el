8245f79afa8bbf74840667b1c024e35be60c1c0c
Shun Ueda <git@shunueda.org>
2026-07-28 01:33:59 -0400

    refactor(emacs): format

--- emacs/init.el
+++ emacs/init.el
@@ -1,44 +1,44 @@
 ;; -*- lexical-binding: t; -*-
 
 (setq
-  ;; keep-sorted start
-  ;; Don't warn on advice
-  ad-redefinition-action 'accept
-  ;; Don't show the startup message
-  auto-save-default nil
-  completion-cycle-threshold t
-  confirm-kill-emacs 'yes-or-no-p
-  custom-file (concat user-emacs-directory "custom.el")
-  ;; Revert Dired and other buffers
-  global-auto-revert-non-file-buffers t
-  indent-tabs-mode nil
-  ;; Don't show the startup message
-  inhibit-startup-message t
-  ;; Don't warn on large files
-  large-file-warning-threshold nil
-  ;; Make super-save work nicely with Magit
-  ;; https://github.com/bbatsov/super-save/issues/7#issuecomment-872380612
-  magit-save-repository-buffers 'dontask
-  ;; No backup files
-  make-backup-files nil
-  markdown-display-remote-images t
-  markdown-enable-math t
-  ;; Silence compiler warnings as they can be pretty disruptive
-  native-comp-async-report-warnings-errors nil
-  read-buffer-completion-ignore-case t
-  read-file-name-completion-ignore-case t
-  ;; Make it easy to cycle through previous items in the mark ring
-  set-mark-command-repeat-pop t
-  tab-always-indent 'complete
-  tab-width 2
-  treesit-font-lock-level 4
-  use-dialog-box nil
-  use-package-always-ensure nil
-  ;; Follow symlinks to VC-controlled files without warning
-  vc-follow-symlinks t
-  visible-bell t
-  ;; keep-sorted end
-  )
+ ;; keep-sorted start
+ ;; Don't warn on advice
+ ad-redefinition-action 'accept
+ ;; Don't show the startup message
+ auto-save-default nil
+ completion-cycle-threshold t
+ confirm-kill-emacs 'yes-or-no-p
+ custom-file (concat user-emacs-directory "custom.el")
+ ;; Revert Dired and other buffers
+ global-auto-revert-non-file-buffers t
+ indent-tabs-mode nil
+ ;; Don't show the startup message
+ inhibit-startup-message t
+ ;; Don't warn on large files
+ large-file-warning-threshold nil
+ ;; Make super-save work nicely with Magit
+ ;; https://github.com/bbatsov/super-save/issues/7#issuecomment-872380612
+ magit-save-repository-buffers 'dontask
+ ;; No backup files
+ make-backup-files nil
+ markdown-display-remote-images t
+ markdown-enable-math t
+ ;; Silence compiler warnings as they can be pretty disruptive
+ native-comp-async-report-warnings-errors nil
+ read-buffer-completion-ignore-case t
+ read-file-name-completion-ignore-case t
+ ;; Make it easy to cycle through previous items in the mark ring
+ set-mark-command-repeat-pop t
+ tab-always-indent 'complete
+ tab-width 2
+ treesit-font-lock-level 4
+ use-dialog-box nil
+ use-package-always-ensure nil
+ ;; Follow symlinks to VC-controlled files without warning
+ vc-follow-symlinks t
+ visible-bell t
+ ;; keep-sorted end
+ )
 
 ;; Local custom file if possible
 (when (file-exists-p custom-file)
@@ -144,15 +144,15 @@
 (use-package gptel
   :config
   (setq
-     gptel-backend
-       (gptel-make-openai "OpenRouter"
-         :host "openrouter.ai"
-         :endpoint "/api/v1/chat/completions"
-         :stream t
-         :key (lambda ()
-                (auth-source-pass-get 'secret "ApiKeys/Openrouter"))
-         :models '(openrouter/auto))
-     gptel-model 'openrouter/auto))
+   gptel-backend
+   (gptel-make-openai "OpenRouter"
+		      :host "openrouter.ai"
+		      :endpoint "/api/v1/chat/completions"
+		      :stream t
+		      :key (lambda ()
+			     (auth-source-pass-get 'secret "ApiKeys/Openrouter"))
+		      :models '(openrouter/auto))
+   gptel-model 'openrouter/auto))
 
 (use-package avy
   :config
@@ -228,21 +228,33 @@
   (consult-async-input-throttle 0)
   ;; Overlay default keybinds
   :bind (("C-s" . consult-line)
-         ("C-x b" . consult-buffer))
+         ("C-x b" . consult-buffer)))
+
+(use-package project
   :config
   (require 'keymap)
   (require 'cl-seq)
-  (require 'project)
+
   ;; https://github.com/minad/consult/wiki#use-consult-ripgrep-instead-of-project-find-regexp-in-projectel
-  (keymap-substitute project-prefix-map #'project-find-regexp #'consult-ripgrep)
+  (keymap-substitute
+   project-prefix-map
+   #'project-find-regexp
+   #'consult-ripgrep)
   (cl-nsubstitute-if
    '(consult-ripgrep "Find regexp")
-   (pcase-lambda (`(,cmd _)) (eq cmd #'project-find-regexp))
-    project-switch-commands)
-  (keymap-substitute project-prefix-map #'project-find-file #'consult-fd)
+   (pcase-lambda (`(,cmd _))
+     (eq cmd #'project-find-regexp))
+   project-switch-commands)
+
+  ;; consult-fd version of above
+  (keymap-substitute
+   project-prefix-map
+   #'project-find-file
+   #'consult-fd)
   (cl-nsubstitute-if
    '(consult-fd "Find file")
-   (pcase-lambda (`(,cmd _)) (eq cmd #'project-find-file))
+   (pcase-lambda (`(,cmd _))
+     (eq cmd #'project-find-file))
    project-switch-commands))
 
 (use-package direnv
@@ -261,18 +273,18 @@
   (go-ts-mode . eglot-ensure)
   :config
   (add-to-list 'eglot-server-programs
-               '(nix-ts-mode . ("nixd")))
+	       '(nix-ts-mode . ("nixd")))
   (add-to-list 'eglot-server-programs
-               '(python-ts-mode . ("pylsp")))
+	       '(python-ts-mode . ("pylsp")))
   (add-to-list 'eglot-server-programs
-               '(go-ts-mode . ("gopls")))
+	       '(go-ts-mode . ("gopls")))
   (add-to-list 'eglot-server-programs
-               '(kotlin-ts-mode . ("kotlin-language-server")))
+	       '(kotlin-ts-mode . ("kotlin-language-server")))
   (add-to-list 'eglot-server-programs
-             '((rust-ts-mode rust-mode) .
-               ("rust-analyzer" :initializationOptions (:check (:command "clippy")))))
+	       '((rust-ts-mode rust-mode) .
+		 ("rust-analyzer" :initializationOptions (:check (:command "clippy")))))
   (add-to-list 'completion-category-overrides
-             '(eglot (styles orderless))))
+	       '(eglot (styles orderless))))
 (use-package wgrep
   :custom
   (wgrep-auto-save-buffer t))
